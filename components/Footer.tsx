526330f61e0e8ec1444757bb4a208be0d58553f9
Chuan Yue <chuanyue@Chuans-MacBook-Pro.local>
2025-10-04 12:29:59 +0800

    Update footer copyright to Noetex Ltd

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -113,7 +113,7 @@ export default function Footer() {
         <div className="border-t border-white/10 mt-12 pt-8">
           <div className="flex flex-col md:flex-row justify-between items-center">
             <p className="text-gray-400 text-sm">
-              © {currentYear} Noetex Academy. All rights reserved.
+              © {currentYear} Noetex Ltd. All rights reserved.
             </p>
             <div className="flex space-x-6 mt-4 md:mt-0">
               {footerLinks.support.map((link) => (
